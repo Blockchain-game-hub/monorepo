63d732fcb58f3898d41df6be282be1e683d52aea
0xkhwarizmi <0xkhwarizmi@gmail.com>
2021-11-17 17:03:03 -0500

    Navbar

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,7 +1,22 @@
 import React from "react";
-import { Flex, Text, Button, Box } from "@chakra-ui/react";
+import {
+  Flex,
+  Icon,
+  Button,
+  Box,
+  Menu,
+  Text,
+  MenuButton,
+  MenuList,
+  MenuItem,
+} from "@chakra-ui/react";
+import Gravatar from "react-gravatar";
 import { useWalletContext } from "../context/wallet";
 import Link from "next/link";
+import { IoDiamondOutline, IoGridOutline } from "react-icons/io5";
+import { FiLogOut, FiHelpCircle } from "react-icons/fi";
+import { FaRegUserCircle } from "react-icons/fa";
+import { truncateAddress } from "../utils/address";
 
 const Navbar = () => {
   const walletContext = useWalletContext();
@@ -17,18 +32,73 @@ const Navbar = () => {
     >
       <Box cursor="pointer">
         <Link href="/">
-          <Text fontSize="2em" fontWeight="700">
-            🔮 Portal
-          </Text>
+          <Box width="10em">
+            <img src="/images/logo.svg" />
+          </Box>
         </Link>
       </Box>
       <Box>
         {address ? (
-          <Button color="white" bg="red.400" onClick={disconnect}>
-            Disconnect Wallet
-          </Button>
+          <Flex>
+            <Menu>
+              <MenuButton color="black" bg="white" mr="5" as={Button}>
+                <Flex alignItems="center">
+                  <Gravatar
+                    size="30"
+                    style={{ borderRadius: "999px" }}
+                    email={address}
+                  />
+                  <Text ml="3">{truncateAddress(address)}</Text>
+                </Flex>
+              </MenuButton>
+              <MenuList color="black" bg="white">
+                <MenuItem>
+                  <Icon
+                    w={5}
+                    h={5}
+                    as={FaRegUserCircle}
+                    style={{ marginRight: "1em" }}
+                  />
+                  View Portal
+                </MenuItem>
+                <MenuItem>
+                  <Icon
+                    as={IoGridOutline}
+                    w={5}
+                    h={5}
+                    style={{ marginRight: "1em" }}
+                  />
+                  Manage Portal
+                </MenuItem>
+                <MenuItem>
+                  <Icon
+                    w={5}
+                    h={5}
+                    as={FiHelpCircle}
+                    style={{ marginRight: "1em" }}
+                  />
+                  Help
+                </MenuItem>
+                <MenuItem>
+                  <Icon
+                    w={5}
+                    h={5}
+                    as={FiLogOut}
+                    style={{ marginRight: "1em" }}
+                  />
+                  Disconnect
+                </MenuItem>
+              </MenuList>
+            </Menu>
+            <Button color="black" bg="white" borderRadius="5" onClick={connect}>
+              <Icon as={IoDiamondOutline} style={{ marginRight: "0.5em" }} />{" "}
+              Create
+            </Button>
+          </Flex>
         ) : (
-          <Button onClick={connect}>Connect Wallet</Button>
+          <Button color="black" bg="white" borderRadius="5" onClick={connect}>
+            Connect Wallet
+          </Button>
         )}
       </Box>
     </Flex>
